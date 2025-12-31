00742afc69312b74693b093859d0ff1b93b7b749
skandraajb <skandraajb@gmail.com>
2025-12-31 21:29:08 +0530

    fix from prev commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const db = path.join(__dirname, 'articles.db');
+const db = require('./db');
 const app = express();
 
 app.use(cors());
